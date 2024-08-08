69d904bdd43f4dd84ebf492c0132b5f5f46e6e50
ahazra <ahazra@microsoft.com>
2024-08-08 11:36:25 +0530

    Ship

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -4,7 +4,7 @@ const msalConfig: Configuration = {
   auth: {
     clientId: "b44c64a6-d623-4e05-90a8-56cfe4bcc62c", // Replace with your client ID
     authority: "https://login.microsoftonline.com/4197fcf4-4986-4f7d-852d-3955410dc21f", // Replace with your tenant ID if needed
-    redirectUri: "http://localhost:3000/", // Replace with your redirect URI
+    redirectUri: "https://anihazra678.github.io/msal-naa-test/", // Replace with your redirect URI
     supportsNestedAppAuth: true,
   },
   cache: {
